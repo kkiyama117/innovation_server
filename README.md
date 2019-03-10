c63651ab1d448fec6617a253d6e0a9a009bbec80
kkiyama117 <k.kiyama117@gmail.com>
2019-03-11 01:42:23 +0900

    Update README

--- README.md
+++ README.md
@@ -6,10 +6,12 @@
 ## Attention
 - あくまでテスト用ということで, SSLによるHTTPS化は行っていません.
 こちらについては
-  - Apacheで(鍵暗号のファイルを指定して)直接設定する
-  - AWSのサービス(Route53等)を用いてHTTPSの設定をする
+  - Apacheの`httpd.conf`等で(鍵暗号のファイルを指定して)直接設定する.
+  - AWSのサービス(Route53等)を用いてHTTPSの設定をする.
  等の方法で設定する事になるかと思います.
 - 開発環境の都合等でMYSQLとほぼ同等で互換性のあるMariaDBを利用しています.
+  ただ, 基本的にこのアプリに関しては差は無いと思われます
+    ただ, 基本的にこのアプリに関しては差は無いと思われます.
 
 ## Requirement
 - `npm`(or `yarn`)
@@ -23,10 +25,8 @@
 ## Install
 ### Initialize database
 1. はじめにmysqlのUserを用意する.
-2. `mysql -u (userの名前) -p < ./lib/initialize.sql` を実行する
-3. `utils/database.js` の DBの設定を変更する.(host, username, password)
-
-## Usage
+2. `cp ./.env.example ./.env` を実行し, `.env` の中身を環境に合わせて変更する.
+3. `mysql -u (userの名前) -p < ./lib/initialize.sql` を実行する.
 
 ## Use
 - Apache Web server -> version 2.4.25
@@ -36,4 +36,4 @@
 [MIT]()
 
 ## Author
-[kkiyama117](ht
+[kkiyama117](https://github.com/kkiyama117/)
--- utils/create_responce.js
+++ utils/create_responce.js
@@ -1,18 +1,30 @@
 let db = require('./database');
 
-function fix_float(results) {
-    // fix error caused by IEEE754
-    return results.map(function (value) {
+// fix error caused by IEEE754
+function fix_float(rates) {
+    return rates.map(function (value) {
         value.rate = value.rate.toFixed(2);
         return value;
     });
+}
+
+// parse datetime to date formatted string
+function parse_date(rates) {
+    return rates.map(function (value) {
+        value.start_date = value.start_date.toISOString().substring(0, 10);
+        return value;
+    });
+}
+
+function parse_results(results) {
+    return fix_float(parse_date(results))
 }
 
 // get_all
 exports.get_all = function (req, res) {
     let connection = db.connection;
     connection.execute('select * from tax_rate', [], function (error, results, fields) {
-        res.json(fix_float(results));
+        res.json(parse_results(results));
     });
 };
 
@@ -22,7 +34,7 @@ exports.get = function (req, res) {
     // mysql library named placeholders
     // https://github.com/sidorares/node-mysql2/blob/master/documentation/Extras.md#named-placeholders
     connection.execute('SELECT * FROM tax_rate where id = ?', [req.params.id], function (error, results, fields) {
-        res.json(fix_float(results));
+        res.json(parse_results(results));
     });
 };
 
